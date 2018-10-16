98a3c35ac4523eba701d41ecde357df7ab30e492
Christy Simmons <cmorrisond@gmail.com>
2018-10-16 12:34:03 -0700

    init upload
    init 3 files: html, css, js
    
    (Morrison! Stop using Drive for everything!)

---
+++ sugar.js
@@ -0,0 +1,18 @@
+(function (){
+
+	var title_options = [
+		"decently reliable human organism",
+		"a human who pokes at keyboards",
+		"code minigolfer",
+		"tap tap tap tap tap",
+		"spends a lot of time with flexbox for some reason",
+		"maybe don't global-install that npm package",
+		"document.getElementBySurname",
+		"node minor demon. More of a node angry sprite, really."
+	];
+
+	var titleChoice = title_options[Math.floor(Math.random() * title_options.length)];
+	document.title = "Christina Simmons: " + titleChoice;
+})();
+
+
